c23258343dcaa58edf4218fcf2af4ea867c9855f
Justin Gray <justin.s.gray@gmail.com>
2013-10-15 19:03:43 -0400

    works with boostrap three

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-	summary: "Latest version of X-Editable for Bootstrap with wysihtml5 rich text editor"
+	summary: "X-Editable 1.5.0 for Bootstrap3 with wysihtml5 rich text editor"
 });
 
 Package.on_use(function (api){
@@ -7,10 +7,10 @@ Package.on_use(function (api){
 	api.use(['jquery'], 'client')
 
 	//x-editable
-	api.add_files('lib/bootstrap-editable/css/bootstrap-editable.css', 'client');
-	api.add_files('lib/bootstrap-editable/js/bootstrap-editable.js', 'client', {bare:true});
-	api.add_files('lib/bootstrap-editable/img/clear.png', 'client');
-	api.add_files('lib/bootstrap-editable/img/loading.gif', 'client');
+	api.add_files('lib/bootstrap3-editable/css/bootstrap-editable.css', 'client');
+	api.add_files('lib/bootstrap3-editable/js/bootstrap-editable.js', 'client', {bare:true});
+	api.add_files('lib/bootstrap3-editable/img/clear.png', 'client');
+	api.add_files('lib/bootstrap3-editable/img/loading.gif', 'client');
 	
 	//address
 	api.add_files('lib/address/address.css', 'client');
@@ -22,7 +22,7 @@ Package.on_use(function (api){
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/wysihtml5-0.3.0.js', 'client', {bare:true});
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.css', 'client');
 	api.add_files('lib/wysihtml5/bootstrap-wysihtml5-0.0.2/bootstrap-wysihtml5-0.0.2.js', 'client', {bare:true});
-	
+
 	//override image paths
 	api.add_files('path-override.css', 'client');  
 });
